edb600c6f440e41dd37b3652a91bcbaf8c339560
malakehab2003 <112804762+malakehab2003@users.noreply.github.com>
2025-09-16 22:10:13 +0300

    fix search

--- Front-end/Clothes-app/src/Components/category-page/category-page.component.ts
+++ Front-end/Clothes-app/src/Components/category-page/category-page.component.ts
@@ -81,8 +81,8 @@ export class CategoryPageComponent implements OnInit {
       (!this.search || 
         p.title.toLowerCase().includes(this.search.toLowerCase()) ||
         p.description.toLowerCase().includes(this.search.toLowerCase()) ||
-        p.brand.toLowerCase().includes(this.search.toLowerCase()) ||
-        p.category.toLowerCase().includes(this.search.toLowerCase())
+        (p.brand || "").toLowerCase().includes(this.search.toLowerCase()) ||
+        (p.category || "").toLowerCase().includes(this.search.toLowerCase())
       )&&
       p.price <= this.maxPrice
     );
